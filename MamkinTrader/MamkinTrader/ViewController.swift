b00cdbd2187c90b2822fdc67107aac7c815165b9
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-29 11:22:07 +0300

    Initial Project

---
+++ MamkinTrader/MamkinTrader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MamkinTrader
+//
+//  Created by Oleg Ryasnoy on 29.05.2018.
+//  Copyright © 2018 Mamkin-Trader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
